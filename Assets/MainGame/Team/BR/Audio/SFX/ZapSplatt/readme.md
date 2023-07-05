acf84de4833c75f4b5cc3d7002b0b0d57f84aaca
Bernhard_Rubow <brubow@newvisionsproject.de>
2023-07-05 07:52:03 +0200

    Paddle Sound and Line Bounce added

---
+++ Assets/MainGame/Team/BR/Audio/SFX/ZapSplatt/readme.md
@@ -0,0 +1,11 @@
+# Folder-Content
+## Description
+This Folder contains Sound Effects downloaded from ZapSplat
+
+## License
+Attribution / Credit Required (Basic / Free Account)
+Sound effects licensed via this standard license require attribution in the form of a credit / link back to our website. If your works containing our sound effects are published on multiple platforms or services, the credit must appear on each where our sounds are used. Please do NOT credit us from any social media platform, unless your work is ONLY being released via a social media platform and it’s the only place you can credit us. Credits must be added to the project itself, for example in your YouTube video description, app or game credits page or your projects related website or blog etc. We are fairly flexible on how you do this, but we recommend something like:
+
+- “Sound effects obtained from https://www.zapsplat.com“
+- “Additional sound effects from https://www.zapsplat.com“
+- “Music from https://www.zapsplat.com“
--- Assets/MainGame/Team/BR/Code/Scripts/Controller_SoundEffects.cs
+++ Assets/MainGame/Team/BR/Code/Scripts/Controller_SoundEffects.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Controller_SoundEffects : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] m_BounceSounds;
+    [SerializeField] private AudioClip[] m_PaddleBounceSounds;
+    [SerializeField] private AudioClip[] m_LineBounceSounds;
     [SerializeField] private AudioSource m_AudioSource;
 
     // +++ Unity event functions ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -18,18 +19,26 @@ public class Controller_SoundEffects : MonoBehaviour
     void OnEnable()
     {
         MessageBus.Subscribe<Message_PaddleHit>(OnPaddleHit);
+        MessageBus.Subscribe<Message_SideLineHit>(OnSideLineHit);
     }
 
     void OnDisable()
     {
         MessageBus.UnSubscribe<Message_PaddleHit>(OnPaddleHit);
+        MessageBus.UnSubscribe<Message_SideLineHit>(OnSideLineHit);
     }
 
 
     // +++ custom event handler +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private void OnSideLineHit(object obj)
+    {
+        var clipToPlay = m_LineBounceSounds.GetRandom();
+        m_AudioSource.PlayOneShot(clipToPlay);
+    }
+
     private void OnPaddleHit(object eventArgs)
     {
-        var clipToPlay = m_BounceSounds.GetRandom();
+        var clipToPlay = m_PaddleBounceSounds.GetRandom();
         m_AudioSource.PlayOneShot(clipToPlay);
     }
 }
